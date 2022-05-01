e5efc662eee95896c6f8730b6f2a6d7d7da54005
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-01 12:43:13 +0900

    feat: add token types

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+//go:generate go run golang.org/x/tools/cmd/stringer -type Type -linecomment
+type Type int
+
+const (
+	ILLEGAL Type = iota // ILLEGAL
+	EOF                 // EOF
+
+	// 識別子, リテラル
+	IDENT // IDENT
+	INT   // INT
+
+	// 演算子
+	ASSIGN // =
+	PLUS   // PLUS
+
+	// デリミタ
+	COMMA     // ,
+	SEMICOLON // ;
+	LPAREN    // (
+	RPAREN    // )
+	LBRACE    // {
+	RBRACE    // }
+
+	// キーワード
+	FUNCTION // FUNCTION
+	LET      // LET
+)
+
+type Token struct {
+	Type    Type
+	Literal string
+}
