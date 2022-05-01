c04a3f87c409ce587fdbc022eb7db791033c18b4
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-01 13:21:18 +0900

    refactor: refactored test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,6 +1,7 @@
 package lexer_test
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/Warashi/implement-interpreter-with-go/lexer"
@@ -10,32 +11,28 @@ import (
 
 func TestNextToken(t *testing.T) {
 	const input = `=+(){},;`
+	tok := func(t token.Type, l string) token.Token { return token.Token{Type: t, Literal: l} }
 
-	tests := []struct {
-		name  string
-		input string
-		waste int
-		want  token.Token
-	}{
-		{name: token.ASSIGN.String(), waste: 0, want: token.Token{token.ASSIGN, "="}},
-		{name: token.PLUS.String(), waste: 1, want: token.Token{token.PLUS, "+"}},
-		{name: token.LPAREN.String(), waste: 2, want: token.Token{token.LPAREN, "("}},
-		{name: token.RPAREN.String(), waste: 3, want: token.Token{token.RPAREN, ")"}},
-		{name: token.LBRACE.String(), waste: 4, want: token.Token{token.LBRACE, "{"}},
-		{name: token.RBRACE.String(), waste: 5, want: token.Token{token.RBRACE, "}"}},
-		{name: token.COMMA.String(), waste: 6, want: token.Token{token.COMMA, ","}},
-		{name: token.SEMICOLON.String(), waste: 7, want: token.Token{token.SEMICOLON, ";"}},
+	wants := []token.Token{
+		tok(token.ASSIGN, "="),
+		tok(token.PLUS, "+"),
+		tok(token.LPAREN, "("),
+		tok(token.RPAREN, ")"),
+		tok(token.LBRACE, "{"),
+		tok(token.RBRACE, "}"),
+		tok(token.COMMA, ","),
+		tok(token.SEMICOLON, ";"),
 	}
 
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
+	for waste, want := range wants {
+		waste, want := waste, want
+		t.Run(strconv.Itoa(waste), func(t *testing.T) {
 			l := lexer.New(input)
-			for i := 0; i < tt.waste; i++ {
+			for i := 0; i < waste; i++ {
 				l.NextToken()
 			}
 			got := l.NextToken()
-			assert.Equal(t, got, tt.want)
+			assert.Equal(t, got, want)
 		})
 
 	}
