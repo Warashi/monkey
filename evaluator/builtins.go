b1361dd00f0fc360707eb98fe1ff3e5d07243613
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-04 18:45:39 +0900

    feat: implement builtin array modification functions

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -3,7 +3,10 @@ package evaluator
 import "github.com/Warashi/implement-interpreter-with-go/object"
 
 var builtins = map[string]object.Builtin{
-	"len": {Fn: builtinLen},
+	"len":   {Fn: builtinLen},
+	"first": {Fn: builtinFirst},
+	"last":  {Fn: builtinLast},
+	"rest":  {Fn: builtinRest},
 }
 
 func builtinLen(args ...object.Object) object.Object {
@@ -18,4 +21,65 @@ func builtinLen(args ...object.Object) object.Object {
 	default:
 		return newErrorf("argument to `len` not supported, got %s", args[0].Type())
 	}
+}
+
+func builtinFirst(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newErrorf("wrong number of arguments. got=%d, want=%d", len(args), 1)
+	}
+	if args[0].Type() != object.TypeArray {
+		return newErrorf("argument to `first` not supported, got %s", args[0].Type())
+	}
+	arr := args[0].(object.Array)
+	if len(arr.Elements) == 0 {
+		return NULL
+	}
+	return arr.Elements[0]
+}
+
+func builtinLast(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newErrorf("wrong number of arguments. got=%d, want=%d", len(args), 1)
+	}
+	if args[0].Type() != object.TypeArray {
+		return newErrorf("argument to `first` not supported, got %s", args[0].Type())
+	}
+	arr := args[0].(object.Array)
+	if len(arr.Elements) == 0 {
+		return NULL
+	}
+	return arr.Elements[len(arr.Elements)-1]
+}
+
+func builtinRest(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newErrorf("wrong number of arguments. got=%d, want=%d", len(args), 1)
+	}
+	if args[0].Type() != object.TypeArray {
+		return newErrorf("argument to `first` not supported, got %s", args[0].Type())
+	}
+	arr := args[0].(object.Array)
+	if len(arr.Elements) == 0 {
+		return NULL
+	}
+	elements := make([]object.Object, len(arr.Elements)-1)
+	copy(elements, arr.Elements[1:])
+	return object.Array{Elements: elements}
+}
+
+func builtinPush(args ...object.Object) object.Object {
+	if len(args) != 2 {
+		return newErrorf("wrong number of arguments. got=%d, want=%d", len(args), 2)
+	}
+	if args[0].Type() != object.TypeArray {
+		return newErrorf("argument to `first` not supported, got %s", args[0].Type())
+	}
+	arr := args[0].(object.Array)
+	if len(arr.Elements) == 0 {
+		return NULL
+	}
+	elements := make([]object.Object, len(arr.Elements), len(arr.Elements)+1)
+	copy(elements, arr.Elements)
+	elements = append(elements, args[1])
+	return object.Array{Elements: elements}
 }
